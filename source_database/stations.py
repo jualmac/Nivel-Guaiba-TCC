23d4e87430ae69d0c15c40f4cf6f686e5d5ffcaa
Juju <juju@pop-os.localdomain>
2024-11-07 20:08:38 -0300

    Transformed db_handler into a Class for ease of use. Trained access to API using the station names. Cleaned dependencies

--- source_database/stations.py
+++ source_database/stations.py
@@ -3,6 +3,7 @@ import json
 import requests
 import pandas as pd
 from source_database.auth import get_auth
+from source_database.db_handler import connect_to_database, write_dataframe, close_connection
 
 # Get proper HidroWeb Token;
 token = get_auth()
@@ -20,9 +21,18 @@ response = requests.get(url, headers=headers)
 
 # Request Reponse;
 if response.status_code == 200:
-    print("Response data:", response.json())
+    stations = response.json()
+    with open('./data/stations.json', 'w') as file:
+        json.dump(stations, file, indent=4)
+    print('Station data colected!')
+
+    #Turn station json into a proper dataframe;
+    df = pd.DataFrame(stations['items'])
+
+    db_connection = connect_to_database()
+    dataframe = write_dataframe(df, 'stations')
+    close_connection(db_connection)
 else:
     print("Request failed with status code:", response.status_code)
     print("Response text:", response.text)
-
 print('DONE')
--- test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-from source_backend.best_model import best_model
-
-a = pd.DataFrame([[1,2,3], ['a','b','c']])
-
-print(a)
-
