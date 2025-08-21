ca8efa91adb5eb3bf6dafe8483a1bd02f7add67d
Juju <almeidajulianojm@gmail.com>
2025-08-21 08:31:13 -0300

    Changed file names

--- source_database/sub_basins.py
+++ source_database/sub_basins.py
@@ -3,7 +3,7 @@ import json
 import requests
 import pandas as pd
 from source_database.auth import get_auth
-from source_database.db_handler import SQLite_Handler
+from source_database.db_handler_sqlite import SQLite_Handler
 
 # Get proper HidroWeb Token;
 token = get_auth()
